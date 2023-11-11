f5495d2da0f41e2c37eb9b30a420450e2c4a3cbb
Exorcistee <66182409+Exorcistee@users.noreply.github.com>
2023-11-11 21:29:26 +0300

    Add files via upload

---
+++ Lw1/lw1.2.pas
@@ -0,0 +1,175 @@
+﻿//Уразаев Константин, ПС-22
+//Вариант 14.
+//В некоторых строках текстового файла имеются выражения,
+//состоящие   из   двух   целых   чисел,   разделенных    знаком
+//арифметической   операции ('+','-','*','/'). Первое  из  чисел
+//может быть отрицательным. В строке может содержаться несколько
+//выражений. Перед  выражением  и  после него  могут  находиться
+//произвольные символы. Требуется  выделить  строку,  в  которой
+//значение выражения максимально. Вывести найденное максимальное
+//значение (7).
+FUNCTION GetInt(Number: STRING): REAL;
+  VAR
+    L, I, Num, Ten, Finish: INTEGER;
+    Minus: BOOLEAN;
+  BEGIN
+    Num := 0;
+    Ten := 1;
+    IF Number[1] = '-'
+    THEN
+      Minus := TRUE
+    ELSE
+      Minus := FALSE;
+    IF Minus = TRUE
+    THEN
+      Finish := 2
+    ELSE
+      Finish := 1;
+    FOR L := LENGTH(Number) DOWNTO Finish
+    DO
+      BEGIN
+        Ten := 1;
+        FOR I := 1 TO (LENGTH(Number) - L)
+        DO
+          Ten := 10 * Ten;  
+        Num := Num + ((Ord(Number[L]) - 48) * Ten)
+      END;
+    IF Minus = TRUE
+    THEN
+      GetInt := Num * -1
+    ELSE
+      GetInt := Num
+  END;
+
+VAR
+  F: TEXT;
+  I, J, MaxString, CurString : INTEGER;
+  Max, Sum: REAL;
+  S, Str, Number, Number1, Number2, InputFile: STRING;
+  Sign: CHAR;
+BEGIN
+  READLN(InputFile);
+  TRY 
+    ASSIGN(F, InputFile);
+    RESET(F);
+  EXCEPT
+    BEGIN
+      WRITELN('Файл не существует');
+      EXIT
+    END
+  END;  
+  Max := 0;
+  Number1 := '';
+  Number2 := '';
+  I := 1;
+  MaxString := 1;
+  CurString := 1;
+  WHILE NOT EOF(F)
+  DO
+    BEGIN
+      I := 1;
+      READLN(F, S);
+      WHILE I < LENGTH(S)
+      DO
+        BEGIN
+          Str := '';
+          Number := '';
+          Number1 := '';
+          Number2 := '';
+          WHILE (I < Length(S))
+          DO
+            BEGIN
+              IF (S[I] = '-') AND (I > 1)
+              THEN
+                IF (S[I - 1] IN ['0'..'9']) AND (S[I + 1] IN ['0'..'9'])
+                THEN
+                  BREAK
+                ELSE
+                  BEGIN
+                    Str := Str + S[I];
+                    I := I + 1;
+                    CONTINUE
+                  END
+              ELSE
+                IF (S[I] = '-')
+                THEN
+                  BEGIN
+                    Str := Str + S[I];
+                    I := I + 1;
+                    CONTINUE;
+                  END
+                ELSE
+                  IF (S[I] IN ['*', '+', '/']) AND (I > 1)
+                  THEN
+                    BEGIN
+                      IF S[I + 1] IN ['0'..'9']
+                      THEN
+                        BEGIN
+                          BREAK;
+                        END
+                      ELSE
+                        Str := Str + S[I];
+                        I := I + 1;
+                        CONTINUE
+                    END
+                  ELSE
+                    BEGIN
+                      Str := Str + S[I];
+                      I := I + 1
+                    END
+            END;
+          CASE S[I] OF
+              '+': Sign := '+';
+              '-': Sign := '-'; 
+              '*': Sign := '*';
+              '/': Sign := '/'
+            END;
+          I := I + 1;  
+          J := LENGTH(Str);
+          WHILE (Str[J] IN ['0'..'9']) AND (J >= 1)
+          DO
+            BEGIN
+              Number := Number + Str[J];
+              IF J = 1
+              THEN
+                BREAK;
+              J := J - 1; 
+            END;
+          IF Str[J] = '-'
+          THEN
+            Number := Number + S[J];
+          FOR J := LENGTH(Number) DOWNTO 1
+          DO
+            Number1 := Number1 + Number[J];
+          WHILE (S[I] IN ['0'..'9']) AND (I <= LENGTH(S))
+          DO
+            BEGIN
+              Number2 := Number2 + S[I];
+              IF I = LENGTH(S)
+              THEN
+                BREAK;
+              I := I + 1;
+            END;
+          CASE Sign OF
+            '+': Sum := GetInt(Number1) + GetInt(Number2);
+            '-': Sum := GetInt(Number1) - GetInt(Number2); 
+            '*': Sum := GetInt(Number1) * GetInt(Number2);
+            '/': Sum := GetInt(Number1) / GetInt(Number2);  
+          END;
+          IF Max < Sum 
+          THEN
+            BEGIN
+              Max := Sum;
+              MaxString := CurString;
+            END;
+          WHILE (S[I] NOT IN ['-', '0'..'9']) AND (I < LENGTH(S))
+          DO
+            BEGIN
+              I := I + 1;
+            END;
+        END;    
+        CurString := CurString + 1;  
+      END;
+   WRITELN('Максимальное число - ', Max);
+   WRITELN('Его строка - ', MaxString);
+END.
---
+++ lw2/lw2.pas
@@ -0,0 +1,201 @@
+﻿//Уразаев Константин, ПС-22
+//Вариант 13.
+//13. В файле заданы N упорядоченных по  возрастанию  списков
+//целых чисел.  Организовать  в  оперативной  памяти  с  помощью 
+//указателей N линейных списков и слить их в общий список (8).
+
+TYPE
+  PNode = ^ListNumbers;
+  ListNumbers = RECORD         
+                  Number: INTEGER;
+                  Next: ^ListNumbers;
+                END;
+
+VAR
+  F: TEXT;
+  MergedListNumbers, FirstListNumbers, SecondListNumbers: ^ListNumbers;
+  InputFile, NumberString: STRING;
+  Number: INTEGER;
+  First, S:  BOOLEAN;
+
+PROCEDURE InsertNumber(VAR List: PNode; Number: INTEGER);
+VAR
+  NewNode, Current: PNode;
+BEGIN
+
+  NEW(NewNode);
+  NewNode^.Number := Number;
+  NewNode^.Next := NIL;
+  
+  IF (List = NIL) 
+  THEN
+    BEGIN
+      List := NewNode;
+    END
+  ELSE
+    BEGIN
+      Current := List;
+      WHILE (Current^.Next <> nil)
+      DO
+        BEGIN
+          Current := Current^.Next;
+        END;
+      NewNode^.Next := Current^.Next;
+      Current^.Next := NewNode;
+    END;
+END;
+
+PROCEDURE Merge(VAR MergeList, List1, List2: PNode);
+VAR
+  Current, NewNode: PNode;
+BEGIN
+  
+  Current := NIL;
+
+  WHILE (List1 <> NIL) AND (List2 <> NIL) DO
+    BEGIN
+      NEW(NewNode);
+      IF List1^.Number < List2^.Number THEN
+        BEGIN
+          NewNode^.Number := List1^.Number;
+          List1 := List1^.Next;
+        END
+      ELSE
+        BEGIN
+          NewNode^.Number := List2^.Number;
+          List2 := List2^.Next;
+        END;
+
+      NewNode^.Next := NIL;
+      IF MergeList = NIL THEN
+        MergeList := NewNode
+      ELSE
+        Current^.Next := NewNode;
+      Current := NewNode;
+    END;
+
+  WHILE List1 <> NIL DO
+    BEGIN
+      NEW(NewNode);
+      NewNode^.Number := List1^.Number;
+      NewNode^.Next := NIL;
+      IF MergeList = NIL THEN
+        MergeList := NewNode
+      ELSE
+        Current^.Next := NewNode;
+      Current := NewNode;
+      List1 := List1^.Next;
+    END;
+
+  WHILE List2 <> NIL DO
+    BEGIN
+      NEW(NewNode);
+      NewNode^.Number := List2^.Number;
+      NewNode^.Next := NIL;
+      IF MergeList = NIL THEN
+        MergeList := NewNode
+      ELSE
+        Current^.Next := NewNode;
+      Current := NewNode;
+      List2 := List2^.Next;
+    END;
+END;
+
+PROCEDURE PrintList(List: ^ListNumbers);
+BEGIN
+  WHILE List <> NIL
+  DO
+    BEGIN
+      WRITE(List^.Number, ' ');
+      List := List^.Next;
+    end;
+  WRITELN;  
+END;
+
+PROCEDURE ClearList(VAR List: ^ListNumbers);
+VAR
+  Curr, NextNode: ^ListNumbers;
+BEGIN
+  Curr := List;
+  WHILE Curr <> NIL 
+  DO
+    BEGIN
+      NextNode := Curr^.Next;
+      DISPOSE(Curr);
+      Curr := NextNode;
+    END;
+  List := NIL;
+END;
+
+
+BEGIN
+  WHILE InputFile <> '0'
+  DO
+    BEGIN
+      MergedListNumbers := NIL;
+      FirstListNumbers := NIL;
+      S := FALSE;
+      WRITE('Введите имя файла (для выхода введите 0): ');
+      READLN(InputFile);
+      IF InputFile = '0'
+      THEN
+        BREAK;
+      TRY
+        ASSIGN(F, InputFile);
+        RESET(F);
+      EXCEPT
+        WRITELN('Файла не существует');
+        CONTINUE
+      END;
+      First := TRUE;
+      WHILE NOT EOF(F) 
+      DO
+        BEGIN
+          NumberString := '';
+          SecondListNumbers := NIL;
+          READLN(F, NumberString);
+          WHILE Length(NumberString) > 0 
+          DO
+            BEGIN
+              IF POS(' ', NumberString) > 0
+              THEN
+                BEGIN
+                  Number := StrToInt(Copy(NumberString, 1, POS(' ', NumberString) - 1));
+                  DELETE(NumberString, 1, POS(' ', NumberString))
+                END
+              ELSE
+                BEGIN
+                  Number := StrToInt(NumberString);
+                  NumberString := ''
+                END;
+              IF (First = TRUE) AND (S = FALSE)
+              THEN
+                InsertNumber(FirstListNumbers, Number)
+              ELSE
+                InsertNumber(SecondListNumbers, Number);
+            END;
+          IF First = TRUE
+          THEN
+            BEGIN
+              First := FALSE;
+            END
+          ELSE
+            First := TRUE;
+          S := TRUE;
+          IF (SecondListNumbers <> NIL) = TRUE
+          THEN
+            BEGIN
+              Merge(MergedListNumbers, FirstListNumbers, SecondListNumbers);
+              FirstListNumbers := MergedListNumbers;
+              ClearList(SecondListNumbers);
+              SecondListNumbers := NIL;
+              MergedListNumbers := NIL;
+            end;
+        END;
+      CLOSE(F);
+      PrintList(FirstListNumbers);
+      ClearList(FirstListNumbers);
+      ClearList(SecondListNumbers);
+      ClearList(MergedListNumbers);
+    END
+END.
